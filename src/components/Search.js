9f649ad6bcba22429b798ec8b68e18dddff146e0
sowmya <sowmya19454@gmail.com>
2023-03-17 21:37:40 +0530

    destructing search note component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MdSearch } from 'react-icons/md';
 
-export const Search = ({handleSearchNote}) => {
+export const Search = ({ handleSearchNote}) => {
   return (
     <div className='search'>
         <MdSearch className = "search-icons" size = '1.3em'></MdSearch>
